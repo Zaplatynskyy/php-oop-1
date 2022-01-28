92d8f27c1a3432890d48ef28405d941a2f442027
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-01-28 16:31:10 +0100

    definizione class Movie, con proprietà e metodi

--- classes/Movies.php
+++ classes/Movies.php
@@ -7,6 +7,67 @@
 // vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 
 
+class Movie {
 
+    //--------- PROPERTIES
+
+    public $title;
+    public $duration;
+    public $language;
+    public $overwiev;
+    public $release_date;
+
+
+    //--------- METHODS
+
+    //--------- construct function
+    public function __construct($_title)
+    {
+        $this->title = $_title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    //--------- set and get functions
+    public function setDuration($_duration)
+    {
+        $this->duration = $_duration;
+    }
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function setLanguage($_language)
+    {
+        $this->language = $_language;
+    }
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function setOverwiev($_overwiev)
+    {
+        $this->overwiev = $_overwiev;
+    }
+    public function getOverwiev()
+    {
+        return $this->overwiev;
+    }
+
+    public function setReleaseDate($_release_date)
+    {
+        $this->release_date = $_release_date;
+    }
+    public function getReleaseDate()
+    {
+        return $this->release_date;
+    }
+
+}
 
 ?>
